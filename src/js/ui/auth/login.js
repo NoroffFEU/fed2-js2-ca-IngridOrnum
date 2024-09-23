0335e88292cb870a146cd6137ce8b8ceeabd0547
Ingrid Ornum <ingrid.ornum@gmail.com>
2024-09-23 13:09:21 +0200

    testing

--- src/js/ui/auth/login.js
+++ src/js/ui/auth/login.js
@@ -1,4 +1,23 @@
 export async function onLogin(event) {
-const loginBtn = document
+const loginBtn = document.getElementById('login-btn');
+
+loginBtn.addEventListener('click', async function () {
+
+})
+
+    try {
+        const response = await fetch('https://v2.api.noroff.dev/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: emailLogin,
+                password: passwordLogin
+            })
+        });
+    } catch (error) {
+
+    }
 
 }
